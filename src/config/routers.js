ba494e66508bc621404ae130b23671f0eabb886e
YanAmorelli <1404yan@gmail.com>
2023-01-04 12:07:53 -0300

    Created login layout

--- src/config/routers.js
+++ src/config/routers.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from "../pages/login/login";
 import Main from '../pages/main/main';
 import MonthProgress from "../pages/month-progress/month-progress";
 
@@ -16,6 +17,7 @@ function Routers() {
     <RouterRoot>      
       <Route exact path="/" element={<Main />}></Route>
       <Route exact path="/monthlyStatus" element={<MonthProgress />}></Route>
+      <Route exact path="/login" element={<Login />}></Route>
     </RouterRoot>
   )
 }
---
+++ src/pages/login/login.js
@@ -0,0 +1,34 @@
+import "./login.css"
+
+function Login() {
+    return(
+        <main className="main">
+            <div className="container">
+                <div className="box">
+                    <form>
+                        <h1 className="title">Sign In</h1>
+                        <div className="form-field">
+                            <label htmlFor="femail" className="label-form">Email</label>
+                            <input type={"email"} className="input-form"></input>
+                        </div> 
+
+                        <div className="form-field">
+                            <label htmlFor="fpass" className="label-form">Password</label>
+                            <input type={"password"} className="input-form"></input>
+                        </div>
+
+                        <div className="button-form">
+                            <button className="login-button">Log In</button>
+                        </div>
+                        
+                        <div className="button-form">
+                            <p className="linktext-form">Forgot my password</p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Login
